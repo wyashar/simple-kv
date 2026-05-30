a3eac0e5bda03ec853b00ef5dfe2a4450c65dc92
yashar <yasharsarand1@gmail.com>
2026-05-30 18:50:32 -0400

    map impl starts

--- src/kv_store.rs
+++ src/kv_store.rs
@@ -1,39 +1,15 @@
-use std::collections::HashMap;
+const LOAD_FACTOR: f32 = 0.75;
 
-pub struct KvStore {
-    map: HashMap<Vec<u8>, Vec<u8>>,
+pub struct KvStore<K, V> {
+    buckets: Box<[Option<Node<K, V>>]>,
+    size: usize,
 }
 
-pub enum KvStoreResult {
-    Stored,
-    Found(Vec<u8>),
-    Removed(Vec<u8>),
-    NotFound,
+struct Node<K, V> {
+    key: K,
+    value: V,
+    hash: usize,
+    next: Option<Box<Node<K, V>>>,
 }
 
-impl KvStore {
-    pub fn new() -> Self {
-        KvStore {
-            map: HashMap::new(),
-        }
-    }
-
-    pub fn put(&mut self, key: Vec<u8>, value: Vec<u8>) -> KvStoreResult {
-        self.map.insert(key, value);
-        KvStoreResult::Stored
-    }
-
-    pub fn get(&self, key: &[u8]) -> KvStoreResult {
-        match self.map.get(key) {
-            Some(value) => KvStoreResult::Found(value.clone()),
-            None => KvStoreResult::NotFound,
-        }
-    }
-
-    pub fn del(&mut self, key: &[u8]) -> KvStoreResult {
-        match self.map.remove(key) {
-            Some(removed_value) => KvStoreResult::Removed(removed_value),
-            None => KvStoreResult::NotFound,
-        }
-    }
-}
+impl<K, V> KvStore<K, V> {}
