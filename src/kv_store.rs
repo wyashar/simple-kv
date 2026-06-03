152ef6b8c707cf956c86fd5d06fcc70d66cdfc55
yashar <yasharsarand1@gmail.com>
2026-06-03 14:08:33 -0400

    add tombstones

--- src/kv_store.rs
+++ src/kv_store.rs
@@ -8,7 +8,8 @@ const STARTING_CAPACITY: usize = 16;
 const LOAD_FACTOR: f32 = 0.75;
 
 pub struct KvStore<K, V> {
-    size: usize,
+    len: usize,
+    tombstones_count: usize,
     buckets: Box<[Bucket<K, V>]>,
     hasher: FxBuildHasher,
 }
@@ -44,9 +45,73 @@ impl<K: Hash + Eq, V> KvStore<K, V> {
 
     pub fn new() -> Self {
         Self {
-            size: 0,
+            len: 0,
+            tombstones_count: 0,
             buckets: Box::new([const { Bucket::Empty }; STARTING_CAPACITY]),
             hasher: FxBuildHasher::default(),
+        }
+    }
+
+    pub fn len(&self) -> usize {
+        self.len
+    }
+
+    pub fn is_empty(&self) -> bool {
+        self.len == 0
+    }
+
+    pub fn del(&mut self, key: &K) -> Option<V> {
+        let hash: usize = self.hash_key(key);
+
+        let mut probe_dist: usize = 0;
+        loop {
+            let bucket_index: usize = self.get_bucket_index(hash, probe_dist);
+            let bucket: &Bucket<K, V> = &self.buckets[bucket_index];
+
+            match bucket {
+                Empty => return None,
+                Occupied(entry) => {
+                    if entry.hash == hash && entry.key == *key {
+                        self.len -= 1;
+                        self.tombstones_count += 1;
+                        let old =
+                            std::mem::replace(&mut self.buckets[bucket_index], Bucket::Tombstone);
+
+                        if let Occupied(e) = old {
+                            return Some(e.value);
+                        } else {
+                            unreachable!(
+                                "It is guarenteed for this partial function to be a full function!"
+                            );
+                        }
+                    }
+                }
+                Tombstone => {}
+            }
+
+            probe_dist = probe_dist + 1;
+        }
+    }
+
+    pub fn get(&self, key: &K) -> Option<&V> {
+        let hash: usize = self.hash_key(key);
+
+        let mut probe_dist: usize = 0;
+        loop {
+            let bucket_index: usize = self.get_bucket_index(hash, probe_dist);
+            let bucket: &Bucket<K, V> = &self.buckets[bucket_index];
+
+            match bucket {
+                Empty => return None,
+                Occupied(entry) => {
+                    if entry.hash == hash && entry.key == *key {
+                        return Some(&entry.value);
+                    }
+                }
+                Tombstone => {}
+            }
+
+            probe_dist = probe_dist + 1;
         }
     }
 
@@ -54,19 +119,35 @@ impl<K: Hash + Eq, V> KvStore<K, V> {
         let hash: usize = self.hash_key(&key);
 
         let mut probe_dist: usize = 0;
+        let mut tombstone_index: Option<usize> = None;
         loop {
             let bucket_index: usize = self.get_bucket_index(hash, probe_dist);
             let bucket: &mut Bucket<K, V> = &mut self.buckets[bucket_index];
 
-            // TODO: handle the tombstone case
             match bucket {
+                Tombstone => {
+                    if tombstone_index.is_none() {
+                        tombstone_index = Some(bucket_index);
+                    }
+                }
                 Empty => {
-                    self.buckets[bucket_index] = Bucket::Occupied(KvEntry::new(key, value, hash));
+                    self.len += 1;
+                    let write_index: usize = tombstone_index.unwrap_or(bucket_index);
+                    if tombstone_index.is_some() {
+                        self.tombstones_count -= 1;
+                    }
+                    self.buckets[write_index] = Bucket::Occupied(KvEntry::new(key, value, hash));
                     return;
                 }
                 Occupied(entry) => {
                     if entry.hash == hash && entry.key == key {
-                        entry.value = value;
+                        if let Some(t_index) = tombstone_index {
+                            self.buckets[t_index] =
+                                Bucket::Occupied(KvEntry::new(key, value, hash));
+                            self.buckets[bucket_index] = Bucket::Tombstone;
+                        } else {
+                            entry.value = value;
+                        }
                         return;
                     }
                 }
